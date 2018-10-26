eb632b417e8fb22ececa2c43773e1290dd5ae865
System Administrator <shaharcohen1997@gmail.com>
2018-10-26 21:47:11 +0300

    Add css for chat-page

--- public/js/index.js
+++ public/js/index.js
@@ -26,13 +26,18 @@ socket.on('newLocationMessage', message => {
 
 jQuery('#message-form').on('submit', event => {
   event.preventDefault();
+
+  const messageTextBox = jQuery('[name=message]');
+
   socket.emit(
     'createMessage',
     {
       from: 'User',
-      text: jQuery('[name=message]').val()
+      text: messageTextBox.val()
     },
-    () => {}
+    () => {
+      messageTextBox.val('');
+    }
   );
 });
 
@@ -41,14 +46,19 @@ locationButton.on('click', () => {
   if (!navigator.geolocation) {
     return alert('Geolocation not supported by your browser');
   }
+
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(
     position => {
+      locationButton.removeAttr('disabled').text('Send location');
       socket.emit('createLocationMessage', {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude
       });
     },
     () => {
+      locationButton.removeAttr('disabled').text('Send location');
       alert('Unable to fetch location.');
     }
   );
